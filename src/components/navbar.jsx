a81ae31ee44f02c2f5cf189eb55ea60d894a586b
alomac6 <167495912+alomac6@users.noreply.github.com>
2025-02-15 23:38:43 -0600

    made extra correction in navbar such as underline transition for dropdown submenu

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -11,7 +11,7 @@ export default function Navbar() {
       <nav className="bg-gray-700 flex w-full h-auto">
         <div className="flex justify-center w-screen">
           <ul className="flex items-center justify-evenly w-3/4">
-            <li className="text-white text-2xl flex h-16 items-center group  hover:text-black relative"
+            <li className="text-white text-2xl flex h-16 items-center  hover:text-black relative"
             onMouseEnter={()=>setOpenMenu({openMenu, gallery:true})}
             onMouseLeave={()=>setOpenMenu({openMenu, gallery:false})}
             >
@@ -20,14 +20,14 @@ export default function Navbar() {
               </a>
               {openMenu.gallery && (
                 <div className="absolute top-full">
-                  <div className="left-0 bg-gray-700 animate-dropdown-1 mt-1">
-                    <a href="#" className="block px-4 py-2 text-white hover:text-black relative">
-                      Yoyos
+                  <div className="left-0 bg-gray-700 animate-dropdown-1 mt-1 group/item">
+                    <a href="#" className="block px-4 py-2 text-white hover:text-black relative">Yoyos
+                      <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-orange-600 transition-all duration-350 delay-100 group-hover/item:w-full"></span>
                     </a>
                   </div>
-                  <div className="left-0 bg-gray-700 animate-dropdown-2 mt-1">
-                    <a href="#" className="block px-4 py-2 text-white hover:text-black relative">
-                      Events
+                  <div className="left-0 bg-gray-700 animate-dropdown-2 mt-1 group/item">
+                    <a href="#" className="block px-4 py-2 text-white hover:text-black relative">Events
+                      <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-orange-600 transition-all duration-350 delay-100 group-hover/item:w-full"></span>
                     </a>
                   </div>
                 </div>
@@ -41,14 +41,14 @@ export default function Navbar() {
                 </a>
                 {openMenu.tutorials && (
                 <div className="absolute top-full">
-                  <div className="left-0 bg-gray-700 animate-dropdown-1 mt-1">
-                    <a href="#" className="block px-4 py-2 text-white hover:text-black relative">
-                      Basics
+                  <div className="left-0 bg-gray-700 animate-dropdown-1 mt-1 group/item">
+                    <a href="#" className="block px-4 py-2 text-white hover:text-black relative">Basics
+                      <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-orange-600 transition-all duration-350 delay-100 group-hover/item:w-full"></span>
                     </a>
                   </div>
-                  <div className="left-0 bg-gray-700 animate-dropdown-2 mt-1">
-                    <a href="#" className="block px-4 py-2 text-white hover:text-black relative">
-                      Combos
+                  <div className="left-0 bg-gray-700 animate-dropdown-2 mt-1 group/item">
+                    <a href="#" className="block px-4 py-2 text-white hover:text-black relative">Combos
+                      <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-orange-600 transition-all duration-350 delay-100 group-hover/item:w-full"></span>
                     </a>
                   </div>
                 </div>
@@ -56,19 +56,19 @@ export default function Navbar() {
             </li>
               <li className="text-white text-2xl flex h-16 items-center group hover:text-black">
                 <a href="placeholder" className="relative">YoYo Glossary
-                <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-orange-600 transition-all duration-350 group-hover:w-full"></span>
+                  <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-orange-600 transition-all duration-350 group-hover:w-full"></span>
                 </a>
               </li>
               <li className="text-white text-2xl flex h-16 items-center group hover:text-black">
                 <a href="placeholder" className="relative">Events
-                <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-orange-600 transition-all duration-350 group-hover:w-full"></span>
+                  <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-orange-600 transition-all duration-350 group-hover:w-full"></span>
                 </a>
               </li>
           </ul>
           <div className="flex items-center h-16 group"><a href="placeholder" className="relative flex items-center text-white hover:text-black">
               <span className="relative">
                 <UserCircle size={48} />
-                <span className="absolute inset-0 rounded-full border-2 border-transparent group-hover:border-orange-600"></span>
+                  <span className="absolute inset-0 rounded-full border-2 border-transparent group-hover:border-orange-600"></span>
               </span></a></div>
         </div>
       </nav>
